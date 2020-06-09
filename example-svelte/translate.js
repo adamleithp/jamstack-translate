edbeef08b0348ec0296c49a74d3ddbf7a1207ec3
adamleithp <adamleithp@gmail.com>
2020-06-09 08:37:31 +0100

    Updated example README's with new option structure again

--- example-svelte/translate.js
+++ example-svelte/translate.js
@@ -1,14 +1,5 @@
 
 
-// This file is an example of the usage of this package
-// Place this file in the root of your directory,
-// * Input your google API key (or us dotenv)
-// * Choose your target lanuages,
-// * Choose your source directory
-// * Choose your files to translate (file, or glob)
-// * Choose options
-// * * Target translation file to save all translations
-// * * Load Custom transaltion file (do not translate, simply build generated file structure from saved JSON)
 require('dotenv').config()
 const translate = require('../index.js');
 
